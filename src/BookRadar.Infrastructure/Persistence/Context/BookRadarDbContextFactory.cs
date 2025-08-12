e6274552f01eadc66f52be306f6852875a0cf59f
Juan Marcos Hernandez Orozco <marcos.hernandez@onoff.com.co>
2025-08-12 01:10:47 -0500

    first migration

---
+++ src/BookRadar.Infrastructure/Persistence/Context/BookRadarDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BookRadar.Infrastructure.Persistence.Context;
+
+public sealed class BookRadarDbContextFactory : IDesignTimeDbContextFactory<BookRadarDbContext>
+{
+    public BookRadarDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BookRadarDbContext>();
+
+        const string cs = "Server=(localdb)\\MSSQLLocalDB;Database=BookRadarDb.Dev;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+        optionsBuilder.UseSqlServer(cs);
+
+        return new BookRadarDbContext(optionsBuilder.Options);
+    }
+}
